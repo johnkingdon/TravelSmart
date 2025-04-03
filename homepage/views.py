8b2bb4d2d338c2453136fe3785f8efa196e1209d
johnkingdon <johnkingdon2005@gmail.com>
2025-04-02 23:10:50 -0400

    renamed "main" folder to "homepage"

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, 'main/home.html')
+    return render(request, 'homepage/home.html')
--- travelsmart/urls.py
+++ travelsmart/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', include('homepage.urls')),
     path('accounts/', include('accounts.urls')),
     path('explore/', include('explore.urls')),
 ]
