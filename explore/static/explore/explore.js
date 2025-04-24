d2a982daaf6560a141a1148d5645718799268bf3
johnkingdon <johnkingdon2005@gmail.com>
2025-04-23 22:03:51 -0400

    attractions with no price_level show when price level 1 filter is selected

--- explore/static/explore/explore.js
+++ explore/static/explore/explore.js
@@ -135,7 +135,8 @@ function findNearby(location, category, priceFilter = '') {
       if (completed === typesToSearch.length) {
         let unique = deduplicatePlaces(allResults);
         if (priceFilter) {
-          unique = unique.filter(place => place.price_level == priceFilter);
+          console.log(unique);
+          unique = unique.filter(place => place.price_level == priceFilter || (priceFilter == 1 && place.prince_level == null));
         }
         const sampled = shuffle(unique).slice(0, 20);
         displayResults(sampled);
@@ -144,7 +145,6 @@ function findNearby(location, category, priceFilter = '') {
     });
   });
 }
-
 
 function deduplicatePlaces(places) {
   const seen = new Set();
